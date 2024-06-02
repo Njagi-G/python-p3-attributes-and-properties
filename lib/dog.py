a209e3eff1a2cfb28a14ac0593c9bd76b38f816d
Njagi Githaiga <njagigithaiga@gmail.com>
2024-06-03 00:16:11 +0300

    Add attributes and properties to Dog class and Person class

--- lib/dog.py
+++ lib/dog.py
@@ -13,3 +13,52 @@ APPROVED_BREEDS = [
 
 class Dog:
     pass
+    def __init__(self, name = "Simba", breed="French Bulldog"):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        print("Retrieving name.")
+        return self._name
+
+    def set_name(self, name):
+        if(type(name) == str) and (1<=len(name)<=25):
+            # print(f"Setting name to {name}.")
+            self._name = name
+        
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+
+    def get_breed(self):
+        return self._breed
+ 
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
+
+
+beast = Dog(name = "Beast", breed = "Beagle")
+beast.name
+beast.breed
+
+beauty = Dog(name = "123")
+beauty.name
+beauty.name = "Lassie"
+beauty.name
+beauty.breed = "Pug"
+beauty.breed
+
+bosco = Dog(name = 456, breed = "Alsatian")
+bosco.name = [3, 5, "hello"]
+
+rufus = Dog(name = False)
+
+scar = Dog(breed = "Pitbull")
--- lib/person.py
+++ lib/person.py
@@ -17,3 +17,57 @@ APPROVED_JOBS = [
 
 class Person:
     pass
+    def __init__(self, name = "Leonardo da Vinci", job = "Finance"):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1<=len(name)<=25:
+            name = name.title()
+            self._name = name
+        
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+    
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+
+        else:
+            print("Job must be in list of approved jobs.")
+    
+    job = property(get_job, set_job)
+
+
+pythagoras = Person(name = "Pythagoras", job = "Research & Development")
+print(pythagoras.name)
+print(pythagoras.job)
+
+artist = Person(job = "Marketing")
+print(artist.name)
+print(artist.job)
+
+salesman = Person(name = 123)
+setattr(salesman, "name", "Michael")
+print(salesman.name)
+print(salesman.job)
+
+salesman.job = "Sales"
+print(getattr(salesman, "job"))
+
+coo = Person(name = "Elon", job = "Driver")
+print(coo.name)
+
+setattr(coo, "job", "Admin")
+print(coo.job)
+
+
